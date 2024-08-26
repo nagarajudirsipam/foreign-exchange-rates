19b7fe69bd08d02c484eebfe5ec11438be8ae65b
Nagaraju Dirsipam <nagarajudirsipam@gmail.com>
2024-08-26 21:41:39 +0530

    Add files via upload

--- src/main/java/com/fx/foreign_exchange_rates/controller/ForeignExchangeController.java
+++ src/main/java/com/fx/foreign_exchange_rates/controller/ForeignExchangeController.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -20,6 +21,8 @@ public class ForeignExchangeController {
 
     @GetMapping("/fx")
     public List<ExchangeCurrency> getExchangeRates(@RequestParam(value = "targetCurrency", required = false) String targetCurrency){
+        LocalDate currDate = LocalDate.now();
+        String sourceCurrency = CURRENCY.USD.name();
         if(StringUtils.isNotEmpty(targetCurrency)){
             try {
                 targetCurrency = CURRENCY.valueOf(targetCurrency).name();
@@ -27,16 +30,19 @@ public class ForeignExchangeController {
                 throw new IllegalArgumentException("targetCurency is not a valid one");
             }
         }
-        return foreignExchangeService.getExchangeRates(targetCurrency);
+        return foreignExchangeService.getExchangeRates(sourceCurrency, currDate, targetCurrency);
     }
 
     @GetMapping("/fx/{targetCurrency}")
     public ExchangeCurrencyTimeSeriesResponse getExchangeRatesInTimeSeries(@PathVariable("targetCurrency") String targetCurrency){
+        LocalDate currDate = LocalDate.now();
+        LocalDate threeDaysInPast = currDate.minusDays(3);
+        String sourceCurrency = CURRENCY.USD.name();
         try {
             targetCurrency = CURRENCY.valueOf(targetCurrency).name();
         }catch (Exception exception){
             throw new IllegalArgumentException("targetCurency is not a valid one");
         }
-        return foreignExchangeService.getExchangeRatesInTimeSeries(targetCurrency);
+        return foreignExchangeService.getExchangeRatesInTimeSeries(sourceCurrency, threeDaysInPast, targetCurrency);
     }
 }
