a0dfc7c539227b0a9fae18b9c42f739901b9ce82
Nagaraju Dirsipam <nagarajudirsipam@gmail.com>
2024-08-26 21:45:55 +0530

    Add files via upload

--- src/main/java/com/fx/foreign_exchange_rates/service/ForeignExchangeService.java
+++ src/main/java/com/fx/foreign_exchange_rates/service/ForeignExchangeService.java
@@ -3,10 +3,11 @@ package com.fx.foreign_exchange_rates.service;
 import com.fx.foreign_exchange_rates.model.ExchangeCurrency;
 import com.fx.foreign_exchange_rates.model.ExchangeCurrencyTimeSeriesResponse;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ForeignExchangeService {
-    List<ExchangeCurrency> getExchangeRates(String targetCurrency);
+    List<ExchangeCurrency> getExchangeRates(String sourceCurrency, LocalDate currDate, String targetCurrency);
 
-    ExchangeCurrencyTimeSeriesResponse getExchangeRatesInTimeSeries(String targetCurrency);
+    ExchangeCurrencyTimeSeriesResponse getExchangeRatesInTimeSeries(String sourceCurrency, LocalDate pastDate, String targetCurrency);
 }
